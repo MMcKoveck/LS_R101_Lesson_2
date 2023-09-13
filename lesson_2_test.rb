09b625445f07b99d07d81bce9df8e891edc5843d
MMcKoveck <132276043+MMcKoveck@users.noreply.github.com>
2023-09-13 07:30:02 -0500

    Add files via upload
    Drag and Drop

---
+++ lesson_2_test.rb
@@ -0,0 +1,17 @@
+#=begin
+p 'This is a test'
+#=end
+#=begin
+Michaels-MacBook-Pro:Launch_School balloon$ echo "# LS_R101_Lesson_2" >> README.md
+Michaels-MacBook-Pro:Launch_School balloon$ git init
+Initialized empty Git repository in /Users/balloon/Launch_School/.git/
+Michaels-MacBook-Pro:Launch_School balloon$ git add README.md
+Michaels-MacBook-Pro:Launch_School balloon$ git commit -m "first commit"
+[main (root-commit) cc157fc] first commit
+ 1 file changed, 1 insertion(+)
+ create mode 100644 README.md
+Michaels-MacBook-Pro:Launch_School balloon$ git branch -M main
+Michaels-MacBook-Pro:Launch_School balloon$ git remote add origin https://github.com/MMcKoveck/LS_R101_Lesson_2.git
+Michaels-MacBook-Pro:Launch_School balloon$ git push -u origin main
+#=end
+# THIS IS A fourth ATTEMPT
