f23be4a842d3dc1db538a4a5beeb96b71dcff1b8
MMcKoveck <132276043+MMcKoveck@users.noreply.github.com>
2023-09-13 10:06:30 -0500

    Add files via upload

---
+++ lesson_2_test_2.rb
@@ -0,0 +1,26 @@
+#=begin
+p 'This is a test'
+#=end
+#=begin
+Michaels-MacBook-Pro:Launch_School balloon$ echo "# LS_R101_Lesson_2" >> README.md
+Michaels-MacBook-Pro:Launch_School balloon$ git init
+Initialized empty Git repository in /Users/balloon/Launch_School/.git/
+Michaels-MacBook-Pro:Launch_School balloon$ git add README.md
+Michaels-MacBook-Pro:Launch_School balloon$ git commit -m "first commit"
+[main (root-commit) cc157fc] first commit
+ 1 file changed, 1 insertion(+)
+ create mode 100644 README.md
+Michaels-MacBook-Pro:Launch_School balloon$ git branch -M main
+Michaels-MacBook-Pro:Launch_School balloon$ git remote add origin https://github.com/MMcKoveck/LS_R101_Lesson_2.git
+Michaels-MacBook-Pro:Launch_School balloon$ git push -u origin main
+#=end
+# THIS IS A seventh ATTEMPT
+=begin
+get ready for a lesson of parenthesis.
+Still dragging and dropping.
+=end
+Git: fatal: a branch named 'main' already exists
+1 parent 09b6254 commit ac0a235
+
+Can't' push refs to remote. Try running "Pull" first to integrate your changes.
+can't' publish branch
